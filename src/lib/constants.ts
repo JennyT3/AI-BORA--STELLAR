911fbcb9bb7b8ad4f01dba675c38620b5535e00a
JennyT3 <112415373+JennyT3@users.noreply.github.com>
2026-03-25 22:00:54 +0000

    fix: correct

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,4 @@
-export const WHATSAPP_NUMBER = "351900000000";
+export const WHATSAPP_NUMBER = "351936021747";
 export const WHATSAPP_LINK = `https://wa.me/${WHATSAPP_NUMBER}`;
+export const EMAIL = "helloaibora@proton.me";
+export const EMAIL_LINK = "mailto:helloaibora@proton.me";
