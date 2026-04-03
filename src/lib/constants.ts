05bf8174d513771a8c9e3a2b2f229e1ee5c362ee
JennyT3 <112415373+JennyT3@users.noreply.github.com>
2026-04-03 08:56:14 +0100

    fix: move Google Script URL to constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,3 +2,4 @@ export const WHATSAPP_NUMBER = "351936021747";
 export const WHATSAPP_LINK = `https://wa.me/${WHATSAPP_NUMBER}`;
 export const EMAIL = "helloaibora@proton.me";
 export const EMAIL_LINK = "mailto:helloaibora@proton.me";
+export const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwltQr3n1neMBeGAnoW5MRmkdvh9Zd33ownF_7N9v853fT1g7wgEJOXvj3eAHsyKOAokw/exec";
